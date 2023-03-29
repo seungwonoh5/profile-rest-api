901dcfa8b3bbd2e883bc8b5e1a7086fcb64725d0
seungwonoh5 <soh1@terpmail.umd.edu>
2023-03-28 21:32:55 -0400

    Added user profile feed API

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -5,9 +5,21 @@ class UpdateOwnProfile(permissions.BasePermission):
     """Allow users to edit their own profile"""
 
     def has_object_permission(self, request, view, obj):
-        """Check user is trying to edit their own profile"""
+        """Check if user is trying to edit their own profile"""
 
         if request.method in permissions.SAFE_METHODS:
             return True
         
         return obj.id == request.user.id
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow users to update their own status"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check if user is trying to update their own status"""
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return obj.user_profile.id == request.user.id
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,6 +5,7 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.settings import api_settings
 
 from profiles_api import models
@@ -111,6 +112,12 @@ class HelloViewSet(viewsets.ViewSet):
 
         return Response({"http_method": "DELETE"})
 
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating user profiles"""
     serializer_class = serializers.UserProfileSerializer
@@ -120,8 +127,13 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.SearchFilter, )
     search_fields = ('name', 'email')
 
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handle creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (permissions.UpdateOwnStatus, IsAuthenticated,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
 
-class UserLoginApiView(ObtainAuthToken):
-    """Handle creating user authentication tokens"""
-    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
-
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
